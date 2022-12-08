099276d635d2768e71e6cb7e47981a53be890012
Ezekiel Abodesegun <ezekielphlat@gmail.com>
2022-12-08 21:07:36 +0100

    implement global filter

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import logo from "./logo.svg";
 import "./App.css";
 import BasicTable from "./components/BasicTable";
 import SortingTable from "./components/SortingTable";
+import FilteringTable from "./components/FilteringTable";
 
 function App() {
-  return <SortingTable />;
+  return <FilteringTable />;
 }
 
 export default App;
