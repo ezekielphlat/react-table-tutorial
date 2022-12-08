a23a0d6b20b51078f1d88dd296d916c83a134cd1
Ezekiel Abodesegun <ezekielphlat@gmail.com>
2022-12-08 23:04:40 +0100

    add client size pagination

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import "./App.css";
 import BasicTable from "./components/BasicTable";
 import SortingTable from "./components/SortingTable";
 import FilteringTable from "./components/FilteringTable";
+import PaginationTable from "./components/PaginationTable";
 
 function App() {
-  return <FilteringTable />;
+  return <PaginationTable />;
 }
 
 export default App;
